f6eb086672b8a94329673e365451b5a129889e2c
eposi's mac <eposiM@eposis-MacBook-Air.local>
2026-03-30 19:10:56 -0500

    quick update

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ import com.ecommerce.storage.SavedSessions;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
